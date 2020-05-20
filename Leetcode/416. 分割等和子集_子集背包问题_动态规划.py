dfe5cf8627200e807b2403d19b177c6d72ab3f0f
QDY <qdy960411@outlook.com>
2020-05-20 10:13:37 +0800

    update leetcode_416:Partition_Equal_Subset_Sum

--- Leetcode/416. 分割等和子集_子集背包问题_动态规划.py
+++ Leetcode/416. 分割等和子集_子集背包问题_动态规划.py
@@ -4,7 +4,7 @@
  @Author  : QDY
  @FileName: 416. 分割等和子集_子集背包问题_动态规划.py
 
- 给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
+    给定一个只包含正整数的非空数组。是否可以将这个数组分割成两个子集，使得两个子集的元素和相等。
 
     注意:
     每个数组中的元素不会超过 100
@@ -35,15 +35,18 @@ class Solution:
         # dp[i][j] 表示背包容量为j,装nums[:i]的物品能否恰好装满
         # 若dp[i-1][j]=True,则dp[i][j]=True
         # 否则，dp[i][j] = dp[i-1][j-nums[i]]
+
         length = len(nums)
         # dp_prev = [True]+[False]*sum_  # dp[i][j], 初始i=0
         # dp_cur = [True]+[False]*sum_  # dp[i+1][j], j==0时为True
         dp = [True] + [False] * sum_  # 压缩
+
         for i in range(length):
             for j in range(sum_, nums[i] - 1, -1):  # 从后往前更新，已经更新的不会影响后面更新的
                 dp[j] = dp[j] or dp[j - nums[i]]
-                # for j in range(nums[i],sum_+1):
-            #     dp_cur[j] = dp_prev[j] or dp_prev[j-nums[i]]
-            # dp_prev, dp_cur = dp_cur, [True]+[False]*sum_
+        #          for j in range(nums[i],sum_+1):
+        #             dp_cur[j] = dp_prev[j] or dp_prev[j-nums[i]]
+        #     dp_prev, dp_cur = dp_cur, [True]+[False]*sum_
         # return dp_prev[-1]
+
         return dp[-1]
