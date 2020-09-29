e93c6b06f5362804ab4fb4225688c11c3882332e
QDylan <qdy960411@outlook.com>
2020-09-29 10:33:16 +0800

    add leetcode_Reshape_the_Matrix

---
+++ Leetcode/566. 重塑矩阵.py
@@ -0,0 +1,54 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2020-09-29 10:31
+ @Author  : QDY
+ @FileName: 566. 重塑矩阵.py
+ @Software: PyCharm
+"""
+"""
+在MATLAB中，有一个非常有用的函数 reshape，它可以将一个矩阵重塑为另一个大小不同的新矩阵，但保留其原始数据。
+给出一个由二维数组表示的矩阵，以及两个正整数r和c，分别表示想要的重构的矩阵的行数和列数。
+重构后的矩阵需要将原始矩阵的所有元素以相同的行遍历顺序填充。
+如果具有给定参数的reshape操作是可行且合理的，则输出新的重塑矩阵；否则，输出原始矩阵。
+
+示例 1:
+输入: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 1, c = 4
+输出: 
+[[1,2,3,4]]
+解释:
+行遍历nums的结果是 [1,2,3,4]。新的矩阵是 1 * 4 矩阵, 用之前的元素值一行一行填充新矩阵。
+
+示例 2:
+输入: 
+nums = 
+[[1,2],
+ [3,4]]
+r = 2, c = 4
+输出: 
+[[1,2],
+ [3,4]]
+解释:
+没有办法将 2 * 2 矩阵转化为 2 * 4 矩阵。 所以输出原矩阵。
+
+注意：
+给定矩阵的宽和高范围在 [1, 100]。
+给定的 r 和 c 都是正数。
+
+"""
+
+
+class Solution:
+    def matrixReshape(self, nums, r: int, c: int):
+        if not nums: return nums
+        h, w = len(nums), len(nums[0])
+        if h * w != r * c or (h == r and w == c): return nums
+        res, tmp = [], []
+        for i in range(h):
+            tmp += nums[i]  # 展成一行
+        for i in range(0, len(tmp), c):  # 每行c个元素
+            res.append(tmp[i:i + c])
+        return res
