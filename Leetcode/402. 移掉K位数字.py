6a23e2e2b892b495ac42ed8710d6a0cdcc44df0b
QDylan <qdy960411@outlook.com>
2020-10-13 11:07:45 +0800

    add leetcode_402:Remove_K_Digits

---
+++ Leetcode/402. 移掉K位数字.py
@@ -0,0 +1,46 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2020-10-13 11:06
+ @Author  : QDY
+ @FileName: 402. 移掉K位数字.py
+ @Software: PyCharm
+"""
+"""
+给定一个以字符串表示的非负整数num，移除这个数中的 k 位数字，使得剩下的数字最小。
+
+注意:
+num 的长度小于 10002 且≥ k。
+num 不会包含任何前导零。
+
+示例 1 :
+输入: num = "1432219", k = 3
+输出: "1219"
+解释: 移除掉三个数字 4, 3, 和 2 形成一个新的最小的数字 1219。
+
+示例 2 :
+输入: num = "10200", k = 1
+输出: "200"
+解释: 移掉首位的 1 剩下的数字为 200. 注意输出不能有任何前导零。
+
+示例 3 :
+输入: num = "10", k = 2
+输出: "0"
+解释: 从原数字移除所有的数字，剩余为空就是0。
+
+"""
+
+
+class Solution:
+    def removeKdigits(self, num: str, k: int) -> str:
+        if len(num) <= k: return '0'
+        stack = []  # 维护一个单调递增的栈
+        for i, c in enumerate(num):
+            while stack and k > 0 and int(stack[-1]) > int(c):
+                k -= 1  # 若当前位置的数比后面一个要大，则要删除
+                stack.pop()
+            stack.append(c)
+
+        while stack and k > 0:  # 对于当前的单调栈，若还需要删除，则从后开始删
+            stack.pop()
+            k -= 1
+        return str(int(''.join(stack)))
