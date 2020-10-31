14ba94b3ddfdccf0ff27939d43b7473fdfac499d
QDylan <qdy960411@outlook.com>
2020-10-31 10:27:12 +0800

    add leetcode_606:Construct_String_from_Binary_Tree

---
+++ Leetcode/606. 根据二叉树创建字符串.py
@@ -0,0 +1,58 @@
+# -*- coding: utf-8 -*-
+"""
+ @Time    : 2020-10-31 10:26
+ @Author  : QDY
+ @FileName: 606. 根据二叉树创建字符串.py
+ @Software: PyCharm
+"""
+"""
+你需要采用前序遍历的方式，将一个二叉树转换成一个由括号和整数组成的字符串。
+空节点则用一对空括号 "()" 表示。而且你需要省略所有不影响字符串与原始二叉树之间的一对一映射关系的空括号对。
+
+示例 1:
+
+输入: 二叉树: [1,2,3,4]
+       1
+     /   \
+    2     3
+   /    
+  4     
+
+输出: "1(2(4))(3)"
+
+解释: 原本将是“1(2(4)())(3())”，
+在你省略所有不必要的空括号对之后，
+它将是“1(2(4))(3)”。
+示例 2:
+
+输入: 二叉树: [1,2,3,null,4]
+       1
+     /   \
+    2     3
+     \  
+      4 
+
+输出: "1(2()(4))(3)"
+
+解释: 和第一个示例相似，
+除了我们不能省略第一个对括号来中断输入和输出之间的一对一映射关系。
+
+"""
+
+
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
+class Solution:
+    def tree2str(self, t) -> str:
+        if not t: return ''
+        res = str(t.val)
+        if not t.left and not t.right: return res
+        res += '(%s)' % self.tree2str(t.left)
+        if t.right:
+            res += '(%s)' % self.tree2str(t.right)
+        return res
